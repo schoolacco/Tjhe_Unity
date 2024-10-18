538aa888d0e8f61a83d3f1d9884e5e357f2a921a
schoolacco <anthony.revel510@education.nsw.gov.au>
2024-10-18 16:54:39 +1100

    Uh... ideka

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -13,12 +13,6 @@ public class CheckPoint : MonoBehaviour
 
     #endregion
 
-    
-
-    
-
-    #endregion
-
     #region Static Variables
 
     /// <summary>
@@ -36,14 +30,12 @@ public class CheckPoint : MonoBehaviour
     /// <returns></returns>
     public static Vector3 GetActiveCheckPointPosition()
     {
-        // If player die without activate any checkpoint, we will return a default position
         Vector3 result = new Vector3(0, 0, 0);
 
         if (CheckPointsList != null)
         {
             foreach (GameObject cp in CheckPointsList)
             {
-                // We search the activated checkpoint to get its position
                 if (cp.GetComponent<CheckPoint>().Activated)
                 {
                     result = cp.transform.position;
@@ -57,6 +49,7 @@ public class CheckPoint : MonoBehaviour
 
     #endregion
 
+
     #region Private Functions
 
     /// <summary>
@@ -64,30 +57,23 @@ public class CheckPoint : MonoBehaviour
     /// </summary>
     private void ActivateCheckPoint()
     {
-        // We deactive all checkpoints in the scene
         foreach (GameObject cp in CheckPointsList)
         {
             cp.GetComponent<CheckPoint>().Activated = false;
-            cp.GetComponent<Animator>().SetBool("Active", false);
         }
 
-        // We activated the current checkpoint
         Activated = true;
-        thisAnimator.SetBool("Active", true);
     }
 
     #endregion
 
     void Start()
     {
-
-        // We search all the checkpoints in the current scene
         CheckPointsList = GameObject.FindGameObjectsWithTag("CheckPoint").ToList();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        // If the player passes through the checkpoint, we activate it
         if (other.tag == "Player")
         {
             ActivateCheckPoint();
